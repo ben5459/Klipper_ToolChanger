1c1b46818c060ce22155c6ef0e4ca09ab3e9caf6
ben5459 <104525636+ben5459@users.noreply.github.com>
2022-08-29 14:34:30 -0500

    Update atc.py

--- atc.py
+++ atc.py
@@ -38,7 +38,7 @@ class ATC:
             desc = getattr(self, 'cmd_' + cmd + '_help', None)
             self.gcode.register_command(cmd, func, False, desc)
 
-        self.printer.register_event_handler("klippy:ready", self.Initialize_atc)
+        self.printer.register_event_handler("klippy:connect", "klippy:ready")
         
     cmd_ATC_LOCK_help = "Lock the ATC."
     def cmd_ATC_LOCK(self, gcmd = None):
