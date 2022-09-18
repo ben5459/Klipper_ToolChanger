882bf9818e4ed7c728b0d44af4ef9eacaa9ef0b9
ben5459 <104525636+ben5459@users.noreply.github.com>
2022-09-18 15:32:58 -0500

    Update atc.py

--- atc.py
+++ atc.py
@@ -46,7 +46,7 @@ class ATC:
 
     def ATC(self, ignore_locked = False):
         self.gcode.respond_info("ATC_LOCK running.")
-        if not ignore_locked and #int(self.tool_current) != -1:
+        if not ignore_locked #and int(self.tool_current) != -1:
             self.gcode.respond_info("ATC_LOCK is already locked with tool " + self.tool_current + ".")
         else:
             self.atc_lock_gcode_template.run_gcode_from_command()
